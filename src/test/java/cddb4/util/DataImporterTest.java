42546b55f26e61ed1cf1fb31368615ecb5dca22b
Julien Smeets <j.smeets@quintor.nl>
2016-10-12 11:12:21 +0200

    Add exercise1

--- src/test/java/cddb4/util/DataImporterTest.java
+++ src/test/java/cddb4/util/DataImporterTest.java
@@ -7,10 +7,14 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 /**
  * Created by Julien Smeets (jsmeets@quintor.nl) on 11-10-16.
+ *
+ * TODO: write a unit test that checks whether albums from albums.csv are loaded into the AlbumService
  */
 public class DataImporterTest{
+    private DataImporter dataImporter = new DataImporter();
+
     @Test
-    public void testTest() throws Exception {
+    public void testAlbumLoading() throws Exception {
         assertThat("test", is(equalTo("test")));
     }
 }
